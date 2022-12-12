62e6152ec57b3a4c66f9dd91703ef069d096bbf0
Justin Martin <justinmartin483@outlook.com>
2022-12-12 17:15:27 -0500

    test

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function getAPI() {
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0");
+    xhr.setRequestHeader("X-RapidAPI-Key", "3afd5096e1mshbfca979e6819cc7p187c0ajsn259793b031bd");
+    xhr.setRequestHeader("X-RapidAPI-Host", "visual-crossing-weather.p.rapidapi.com");
+
+    xhr.send(data);
+    console.log(data);
+    console.log(xhr);
+    console.log("test");
+}
+getAPI();
