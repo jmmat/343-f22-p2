ae3669b6fbc4130c2a36719baadbddd8b0aff334
Justin Martin <justinmartin483@outlook.com>
2022-12-12 17:27:24 -0500

    fd

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 function getAPI() {
-    const data = null;
-
-    const xhr = new XMLHttpRequest();
-    //xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-            console.log(this.responseText);
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3afd5096e1mshbfca979e6819cc7p187c0ajsn259793b031bd',
+            'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
         }
-    });
+    };
+    
+    fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=csv&unitGroup=us&shortColumnNames=0', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 
-    xhr.open("GET", "https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0");
-    xhr.setRequestHeader("X-RapidAPI-Key", "3afd5096e1mshbfca979e6819cc7p187c0ajsn259793b031bd");
-    xhr.setRequestHeader("X-RapidAPI-Host", "visual-crossing-weather.p.rapidapi.com");
 
-    xhr.send(data);
-    console.log(data);
-    console.log(xhr);
+    console.log(response);
     console.log("test");
 }
 getAPI();
