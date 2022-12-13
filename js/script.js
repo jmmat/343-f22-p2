18ebe0148c009123625dd004fd51af80715f8ed7
Justin Martin <justinmartin483@outlook.com>
2022-12-12 21:31:22 -0500

    big improvement

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-function getAPI() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '3afd5096e1mshbfca979e6819cc7p187c0ajsn259793b031bd',
-            'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=json&unitGroup=us&shortColumnNames=0', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
 
-    console.log("test3");
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '3afd5096e1mshbfca979e6819cc7p187c0ajsn259793b031bd',
+        'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+    }
+};
+
+function respond(response) {
+    console.log(response);
+    console.log(response.locations);
 }
-getAPI();
+
+const response = fetch('https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=94%20Mill%20Stone%20Drive%2C%20Verona%2CVA&contentType=json&unitGroup=us&shortColumnNames=0', options)
+    .then(response => response.json())
+    .then(response => respond(response))
+    .catch(err => console.error(err));
+
